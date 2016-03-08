0c6c1c6b5ba450441841e1a0e1f17171b12e6b8d
lembarek <lembarek2011@gmail.com>
2016-03-08 17:56:24 +0100

    make the current value is the selected value in enum

--- src/views/edit/partials/enum.blade.php
+++ src/views/edit/partials/enum.blade.php
@@ -1,7 +1,14 @@
+@inject('profile', 'Lembarek\Profile\Repositories\ProfileRepositoryInterface')
+<?php $selected = $profile->getForUser()[$name] ?>
+
  <select name='{{ $name }}' class="select2">
 
     @foreach($schema->get_enum_values('profiles', $name) as $value)
-    <option value="{{$value}}">{{ $value }}</option>
+        @if($value === $selected)
+        <option value="{{$value}}" selected='selected'>{{ $value }}</option>
+        @else
+        <option value="{{$value}}">{{ $value }}</option>
+        @endif
     @endforeach
 
 </select>
