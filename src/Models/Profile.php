55934c0431c18d088a9c0c695689b5d3322f3271
lembarek <lembarek2011@gmail.com>
2016-03-16 14:27:54 +0100

    add user to Models/profile

--- src/Models/Profile.php
+++ src/Models/Profile.php
@@ -7,4 +7,10 @@ class Profile extends Model
     protected $fillable = ['user_id', 'country', 'sex', 'birth_date'];
 
     protected $hidden = ['id', 'user_id','created_at','updated_at'];
+
+    public function  user()
+    {
+        return $this->belongsTo('Lembarek\Auth\Models\User');
+    }
+
 }
